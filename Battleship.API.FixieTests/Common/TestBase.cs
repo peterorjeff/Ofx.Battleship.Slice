c0b904aa830dc8862e810f16aa9161d444dc413d
peterorjeff <peterorjeff@gmail.com>
2020-02-01 16:19:57 +1100

    add first unit test and common classes

---
+++ Battleship.API.FixieTests/Common/TestBase.cs
@@ -0,0 +1,20 @@
+﻿using Ofx.Battleship.API.Data;
+using System;
+
+namespace Ofx.Battleship.API.FixieTests.Common
+{
+    public class TestBase : IDisposable
+    {
+        protected readonly BattleshipDbContext _context;
+
+        public TestBase()
+        {
+            _context = BattleshipDbContextFactory.Create();
+        }
+
+        public void Dispose()
+        {
+            BattleshipDbContextFactory.Destroy(_context);
+        }
+    }
+}
---
+++ Battleship.API.FixieTests/Features/CreateGameTests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using Ofx.Battleship.API.Features.Games;
+using Ofx.Battleship.API.FixieTests.Common;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Battleship.API.FixieTests.Features
+{
+    public class CreateGameTests : TestBase
+    {
+        public async Task Handle_GivenValidRequest_ShouldReturnNewGameId()
+        {
+            // Arrange
+            var handler = new Create.Handler(_context);
+
+            // Act
+            var response = await handler.Handle(new Create.Command(), CancellationToken.None);
+
+            // Assert
+            response.Should().BePositive();
+        }
+    }
+}
