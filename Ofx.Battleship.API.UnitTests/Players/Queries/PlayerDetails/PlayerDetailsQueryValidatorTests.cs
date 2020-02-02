597a5196bdb2d53584bfd2467fac27041c58a5f2
peterorjeff <peterorjeff@gmail.com>
2020-02-02 15:51:10 +1100

    add player detail query validator tests

---
+++ Ofx.Battleship.API.UnitTests/Players/Queries/PlayerDetails/PlayerDetailsQueryValidatorTests.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation.TestHelper;
+using Ofx.Battleship.API.Features.Players.Details;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Ofx.Battleship.API.UnitTests.Players.Queries.PlayerDetails
+{
+    public class PlayerDetailsQueryValidatorTests
+    {
+        private readonly QueryValidator _validator;
+
+        public PlayerDetailsQueryValidatorTests()
+        {
+            _validator = new QueryValidator();
+        }
+
+        [Fact]
+        public void GivenValidPlayerId_ShouldNotHaveValidationError()
+        {
+            _validator.ShouldNotHaveValidationErrorFor(x => x.Id, 1);
+        }
+
+        [Fact]
+        public void GivenInvalidPlayerId_ShouldHaveValidationError()
+        {
+            _validator.ShouldHaveValidationErrorFor(x => x.Id, 0);
+        }
+    }
+}
