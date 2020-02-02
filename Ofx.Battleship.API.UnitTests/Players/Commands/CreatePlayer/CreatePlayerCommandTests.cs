0a4a7a6f5b8ad460987dd7958f97191d6d1afd55
peterorjeff <peterorjeff@gmail.com>
2020-02-02 14:45:44 +1100

    add unit tests for create player command and validator

---
+++ Ofx.Battleship.API.UnitTests/Players/Commands/CreatePlayer/CreatePlayerCommandTests.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using Ofx.Battleship.API.Features.Players.Create;
+using Ofx.Battleship.API.UnitTests.Common;
+using System.Threading;
+using Xunit;
+
+namespace Ofx.Battleship.API.UnitTests.Players.Commands.CreatePlayer
+{
+    public class CreatePlayerCommandTests : CommandTestBase
+    {
+        [Fact]
+        public async void Handle_GivenValidRequest_ShouldReturnNewPlayerId()
+        {
+            // Arrange
+            var command = new Handler(_context);
+
+            // Act
+            var response = await command.Handle(new Command(), CancellationToken.None);
+
+            // Assert
+            response.Should().BePositive();
+        }
+    }
+}
---
+++ Ofx.Battleship.API.UnitTests/Players/Commands/CreatePlayer/CreatePlayerCommandValidatorTests.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation.TestHelper;
+using Ofx.Battleship.API.Features.Players.Create;
+using Xunit;
+
+namespace Ofx.Battleship.API.UnitTests.Players.Commands.CreatePlayer
+{
+    public class CreatePlayerCommandValidatorTests
+    {
+        private readonly CommandValidator _validator;
+
+        public CreatePlayerCommandValidatorTests()
+        {
+            _validator = new CommandValidator();
+        }
+
+        [Fact]
+        public void GivenValidPlayerName_ShouldNotHaveValidationError()
+        {
+            _validator.ShouldNotHaveValidationErrorFor(x => x.Name, "Pete");
+        }
+
+        [Fact]
+        public void GivenEmptyPlayerName_ShouldHaveValidationError()
+        {
+            _validator.ShouldHaveValidationErrorFor(x => x.Name, string.Empty);
+        }
+
+        [Fact]
+        public void GivenInvalidPlayerName_ShouldHaveValidationError()
+        {
+            _validator.ShouldHaveValidationErrorFor(x => x.Name, "this name is really long, much longer than the character limit.");
+        }
+    }
+}
