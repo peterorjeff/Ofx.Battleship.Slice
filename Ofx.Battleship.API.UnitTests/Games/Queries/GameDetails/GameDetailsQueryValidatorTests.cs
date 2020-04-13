9940e31622e7ff58e7846c00e069406dc2ea1b1f
peterorjeff <peterorjeff@gmail.com>
2020-04-13 14:07:49 +1000

    add game details validation tests

--- Ofx.Battleship.API.UnitTests/Games/Queries/GameDetails/GameDetailsQueryValidatorTests.cs
+++ Ofx.Battleship.API.UnitTests/Games/Queries/GameDetails/GameDetailsQueryValidatorTests.cs
@@ -1,10 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using FluentValidation.TestHelper;
+using Ofx.Battleship.API.Features.Games.Details;
+using Xunit;
 
 namespace Ofx.Battleship.API.UnitTests.Games.Queries.GameDetails
 {
-    class GameDetailsQueryValidatorTests
+    public class GameDetailsQueryValidatorTests
     {
+        private readonly QueryValidator _validator;
+
+        public GameDetailsQueryValidatorTests()
+        {
+            _validator = new QueryValidator();
+        }
+
+        [Fact]
+        public void GivenValidGameId_ShouldNotHaveValidationError()
+        {
+            _validator.ShouldNotHaveValidationErrorFor(x => x.Id, 1);
+        }
+
+        [Fact]
+        public void GivenInvalidGameId_ShouldHaveValidationError()
+        {
+            _validator.ShouldHaveValidationErrorFor(x => x.Id, 0);
+        }
     }
 }
